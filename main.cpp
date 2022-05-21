0c9451f537120c00d75bef900ba4a60eadf1a928
Nico <nicodiazan@gmail.com>
2022-05-22 01:02:31 +0200

    Create main.cpp file and add initial parameters

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+//Declare initial variables and parametrs
+
+//X axis 
+const int DIRX = 4;
+const int STEPX = 5;
+//Y axis
+const int DIRY = 6;
+const int STEPY = 7;
+//Z axis
+const int DIRZ = 8;
+const int STEPZ = 9;
+//Tool control
+const int TOOL = 10;
