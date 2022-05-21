673ba06bd3f68a10c161c07489031abcf5dd3767
Nico <nicodiazan@gmail.com>
2022-05-22 01:04:54 +0200

    Add more initial parameters and a setup function

--- main.cpp
+++ main.cpp
@@ -12,3 +12,13 @@ const int DIRZ = 8;
 const int STEPZ = 9;
 //Tool control
 const int TOOL = 10;
+//Steps per revolution
+const int  steps_per_rev = 200;
+//Steps per millimiter for each axis
+const int steps_per_mmX = 0;
+const int steps_per_mmY = 0;
+const int steps_per_mmZ = 0;
+
+
+void setup()
+{}
